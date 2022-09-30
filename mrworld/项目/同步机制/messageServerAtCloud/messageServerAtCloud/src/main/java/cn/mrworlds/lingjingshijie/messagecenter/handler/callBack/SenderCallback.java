8f0972f482d6fd2df6aafc4546a973da38c820cc
“runner99” <2825201723@qq.com>
2022-09-30 18:38:48 +0800

    update

--- mrworld/项目/同步机制/messageServerAtCloud/messageServerAtCloud/src/main/java/cn/mrworlds/lingjingshijie/messagecenter/handler/callBack/SenderCallback.java
+++
@@ -1,8 +0,0 @@
-package cn.mrworlds.lingjingshijie.messagecenter.handler.callBack;
-
-import cn.mrworlds.lingjingshijie.messagecenter.protocol.MRTPCommand;
-
-public interface SenderCallback {
-
-    void callback(MRTPCommand command);
-}
