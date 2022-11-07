e4c5e2753e3951658c6e7cdf49cd652cd983bb4a
“runner99” <2825201723@qq.com>
2022-11-07 23:53:19 +0800

    update

--- java8/src/com/runner99/java8/TestStream.java
+++ java8/src/com/runner99/java8/TestStream.java
@@ -4,7 +4,12 @@ import com.runner99.java8.pojo.User;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * stream流学习
@@ -67,6 +72,78 @@ public class TestStream {
         //并行流输出
 //        list.parallelStream().forEach(System.out::println);
         System.out.println(users);
+    }
+
+
+    //stream结合Predicate表达式返回集合
+    //注意:Predicate完全可以由原始的方法替代，这样可以少一步，详见test01
+    @Test
+    public void test04(){
+        List<User> list = new ArrayList<>();
+        for (int i=0;i<10;i++){
+            list.add(new User(i,"渣渣辉"+i+"号"));
+        }
+        //表示并且的关系
+        Predicate<User> predicate01 = user -> user.getId()>3;
+        Predicate<User> predicate02 = user -> user.getId()<6;
+        List<User> collect = list.stream()
+                .filter(predicate01)
+                .filter(predicate02)
+                .collect(Collectors.toList());
+        System.out.println(collect);
+
+        //表示或者的关系
+        Predicate<User> predicate03 = user -> user.getId()<3;
+        Predicate<User> predicate04 = user -> user.getId()>6;
+        List<User> collect1 = list.stream()
+                .filter(predicate03.or(user -> user.getId()>6))
+                        .collect(Collectors.toList());
+        System.out.println(collect1);
+    }
+
+    //limit的限制数量的使用
+    @Test
+    public void test05(){
+        List<User> list = new ArrayList<>();
+        for (int i=0;i<10;i++){
+            list.add(new User(i,"渣渣辉"+i+"号"));
+        }
+        System.out.println(list.stream().max((num1,num2)->Integer.compare(num1.getId(),num2.getId())).get());
+        List<User> collect = list.stream().limit(3).collect(Collectors.toList());
+        System.out.println(collect);
+    }
+
+    //sorted排序，默认为升序，加入参数Comparator.reverseOrder()为降序
+    @Test
+    public void test06(){
+        List<Integer> list = Arrays.asList(1,7 , 3, 66,-11, 7, 8, 32, 2, 2);
+        //默认升序排序
+//        list.stream().sorted().forEach(str-> System.out.println(str));
+        //降序排序
+//        list.stream().sorted(Comparator.reverseOrder()).forEach(str-> System.out.println(str));
+        //选出最大值
+        System.out.println(list.stream().max(Integer::compareTo).get());
+        //选出最小值
+        System.out.println(list.stream().min(Integer::compareTo).get());
+        //求出长度
+        System.out.println(list.stream().count());
+    }
+
+
+    //map对集合中的元素进行特定的操作，返回可以多个值
+    //reduce按照出入的逻辑进行出来，返回的是一个值
+    @Test
+    public void test07(){
+        List<User> list = new ArrayList<>();
+        for (int i=0;i<10;i++){
+            list.add(new User(i,"渣渣辉"+i+"号"));
+        }
+//        list.stream()
+//                .map(user -> user.getName())
+//                .forEach(System.out::println);
+
+        List<Integer> list2 = Arrays.asList(1,7 , 3, 66,-11, 7, 8, 32, 2, 2);
+        System.out.println(list2.stream().reduce((num, sum) -> num + sum).get());
     }
 
 }
