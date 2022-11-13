e9bef4f0e0e35ee7527705ce293b36551384e65f
“runner99” <2825201723@qq.com>
2022-11-13 23:16:17 +0800

    update

---
+++ timetask/son02/src/main/java/com/example/son02/Son02Application.java
@@ -0,0 +1,13 @@
+package com.example.son02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Son02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Son02Application.class, args);
+    }
+
+}
---
+++ timetask/son02/src/test/java/com/example/son02/Son02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.son02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Son02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
