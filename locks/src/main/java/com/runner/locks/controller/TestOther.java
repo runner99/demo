a98c205b8f5c66fc4ab710e2c03b57435eb81ac4
weizhenqiang <2825201723@qq.com>
2022-11-14 18:53:25 +0800

    update

--- locks/src/main/java/com/runner/locks/controller/TestOther.java
+++ locks/src/main/java/com/runner/locks/controller/TestOther.java
@@ -1,5 +1,6 @@
 package com.runner.locks.controller;
 
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.runner.locks.service.Testservices01;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.xml.crypto.Data;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 @RestController
 public class TestOther {
@@ -43,10 +45,22 @@ public class TestOther {
     @GetMapping("/test/del")
     public String testdel(){
         String begin = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
-        for (int i = 0 ;i<10000;i++ ){
+        for (int i = 0 ;i<100000000;i++ ){
             System.out.println(redisTemplate.delete("woya"+i));
         }
         String end = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
         return begin+"::"+end;
+    }
+    @GetMapping("/test/pressure")
+    public void testde2() throws InterruptedException {
+        while (true){
+            for (int i = 0;i<100;i++){
+                String uuid = String.valueOf(UUID.randomUUID());
+                redisTemplate.opsForValue().set("key"+uuid,i);
+                redisTemplate.delete("keys"+uuid);
+            }
+            Thread.sleep(1000);
+        }
+
     }
 }
