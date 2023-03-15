7d1668c410c65e489b78522e9465ec28a20eef94
“runner99” <74812506+runner99@users.noreply.github.com>
2023-03-16 00:20:35 +0800

    update

---
+++ pgsqlgenerator/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author weizhenqiang
+ * @date ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
