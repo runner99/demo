494e08593ad54d6a5279a77d1c8a195c34a53783
weizhenqiang <2825201723@qq.com>
2023-10-10 14:10:31 +0800

    server_demo项目搭建

---
+++ server_demo/src/main/java/com/runner99/serverDemo/jvm/JvmController.java
@@ -0,0 +1,17 @@
+package com.runner99.serverDemo.jvm;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author weizhenqiang
+ * @date 2023/10/10 14:04
+ */
+@Slf4j
+@RestController
+public class JvmController {
+
+
+
+}
