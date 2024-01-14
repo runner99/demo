a72842aa7a914c27093e7aa45de9411aa7163694
weizhenqiang <2825201723@qq.com>
2024-01-14 15:35:06 +0800

    server_demo新增war包可选配置

--- server_demo/src/main/java/com/runner99/serverDemo/ServletInitializer.java
+++ server_demo/src/main/java/com/runner99/serverDemo/ServletInitializer.java
@@ -3,7 +3,9 @@ package com.runner99.serverDemo;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-
+/**
+ * 打war包时使用
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
