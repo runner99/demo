ef971b4a92747d22c1a352e38724dd594f8410aa
weizhenqiang66 <weizhenqiang@mchz.com.cn>
2024-09-13 18:09:03 +0800

    名称修改

---
+++ mysql_sync/src/main/resources/db/migration/V1.0.0.20240212__create_risk_out.sql
@@ -0,0 +1,6 @@
+CREATE TABLE if NOT EXISTS user_info
+(
+    id               bigint auto_increment
+    primary key,
+    name             varchar(255)             null,
+    );
--- sql_test/src/main/resources/db/migration/V1.0.0.20240213__insert_risk_out.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO test.risk_out (name, out_type, out_ip, out_port, out_protocol, enable,
-                           out_template_id, cron, data_range_start, data_range_end)
-VALUES ('001', null, null, null, null, null, null, null, null, null);
-
-INSERT INTO test.risk_out (name, out_type, out_ip, out_port, out_protocol, enable,
-                           out_template_id, cron, data_range_start, data_range_end)
-VALUES ('002', null, null, null, null, null, null, null, null, null);
